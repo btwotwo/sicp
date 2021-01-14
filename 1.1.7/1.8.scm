41d683011723897aa1758ab8016f194283e8fba2
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-01-14 18:39:46 +0100

    Add 1.8

---
+++ 1.1.7/1.8.scm
@@ -0,0 +1,24 @@
+(define (cubrt-iter prev-guess guess x)
+    (
+        if (good-enough? prev-guess guess)
+            guess
+            (cubrt-iter guess (improve guess x) x)
+        
+    )
+)
+
+(define (good-enough? prev-guess guess) 
+    (< (abs (- guess prev-guess)) 0.0001)
+)
+
+(define (average x y) (
+    / (+ x y y ) 3
+))
+
+(define (improve guess x) (
+    average (/ x (square guess)) guess 
+))
+
+(define (cubrt x) 
+    (cubrt-iter 0 1.0 x)
+)
